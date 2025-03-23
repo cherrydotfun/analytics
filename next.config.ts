fe226f4f88443fd0f23c24fa4a42e6dde3a7e19c
Alex Zverev <alwye@yahoo.com>
2025-03-23 00:48:57 +0000

    next config - ignore all

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,32 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   devIndicators: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      }
+    ]
+  },
 };
 
 export default nextConfig;
