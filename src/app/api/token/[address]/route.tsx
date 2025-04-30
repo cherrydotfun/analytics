ad4b02d1b160213b42162e273db757153b96907c
Nikolai Korablin <korablinn@gmail.com>
2025-04-30 20:26:10 +0300

    Add v1 token checker

---
+++ src/app/api/token/[address]/route.tsx
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { getTopTokenHolders } from '@/lib/token';
+import { getHighScoreAssociations } from '@/lib/wallet-associations';
+import { abbreviateAddress } from '@/lib/formatting';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { tokenAddress: string } }
+) {
+  try {
+    const address = params.tokenAddress;
+
+    const topHoldersResp = await getTopTokenHolders(address);
+
+    // Retrieve associated wallets for the given address.
+    const associations = await getHighScoreAssociations(topHoldersResp.topHolders.map(x => x.address?.address), 1);
+
+    // Build and return the final response.
+    const responseData = {
+      id: address,
+      name: topHoldersResp.tokenName,
+      symbol: topHoldersResp.tokenSymbol,
+      supply: topHoldersResp.tokenSupply,
+      associations
+    };
+
+    return NextResponse.json({data: responseData});
+  } catch (error) {
+    console.error('Error in GET /address/[address_id]:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
