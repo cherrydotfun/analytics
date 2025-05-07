4ce68d3eeed8bf49d572fa6ac2e0634956c3b44e
Nikolai Korablin <korablinn@gmail.com>
2025-05-07 19:05:22 +0300

    loader message

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react"
 export default function Loader(){
     return (
     <div className="flex w-full text-2xl items-center justify-center mt-10 mb-5">
-        <LoaderCircle className="animate-spin mr-4"  /> Loading
+        <LoaderCircle className="animate-spin mr-4"  /> Checking thousands of addresses. This may take a few minutes...
     </div>
     )
 }
--- src/lib/wallet-associations.ts
+++ src/lib/wallet-associations.ts
@@ -1,6 +1,6 @@
 const KB_IP = process.env.CHERRY_KB;
 // number of workers to collect data
-const MAX_PARALLEL = 22;
+const MAX_PARALLEL = 20;
 
 /**
  * 1) Compute association score
